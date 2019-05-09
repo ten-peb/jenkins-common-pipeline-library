6ed1208eaf80ff43764d856147c04d842202a6ef
Peter L. Berghold <pberghold@tenna.com>
2019-05-09 17:48:33 -0400

    Added getUserApproval to the mix

---
+++ vars/getUserApproval.groovy
@@ -0,0 +1,20 @@
+def Boolean call(String, promptid,String prompt_text
+                 String  inquiry_text ) {
+  try {
+    Boolean retval = input( id: promptid,
+      message: prompt_text,
+      [ $class 'BooleanParameterDefinition',
+        defaultValue: false,
+        description: inquiry_text
+      ]
+      )
+   } catch(err) {
+      retval = false 
+   }
+
+   return retval
+}
+
+
+    
+  
