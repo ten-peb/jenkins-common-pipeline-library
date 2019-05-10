0ba07a0bd3e8a4c442b1c477226a7ab7bd77360e
Peter L. Berghold <pberghold@tenna.com>
2019-05-10 14:45:14 -0400

    added Luke and myself to the qa mailing list

--- vars/qaTeam.groovy
+++ vars/qaTeam.groovy
@@ -1,6 +1,7 @@
 def String[] call() {
   def String[] retval = [
        "ashah@tenna.com","khickman@tenna.com","nprabhu@tenna.com"
+       "ldouglas@tenna.com","pberghold@tenna.com"
   ]
 
   return retval
