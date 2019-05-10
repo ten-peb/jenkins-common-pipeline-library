13d55d0de8ed09171ae169d6e6cc3aba9b0e3cd7
Peter L. Berghold <pberghold@tenna.com>
2019-05-10 17:31:38 -0400

    foo

--- vars/qaTeam.groovy
+++ vars/qaTeam.groovy
@@ -1,6 +1,6 @@
 def String[] call() {
   def String[] retval = [
-       "ashah@tenna.com","khickman@tenna.com","nprabhu@tenna.com"
+       "ashah@tenna.com","khickman@tenna.com","nprabhu@tenna.com",
        "ldouglas@tenna.com","pberghold@tenna.com"
   ]
 
